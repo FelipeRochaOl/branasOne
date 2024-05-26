9c059faf568d4a5d64ae07a1bd2d12bead49231a
Felipe Rocha <feliperochaoliveira@gmail.com>
2024-05-26 19:00:07 -0300

    fix: test with yarn

--- jest.config.ts
+++ jest.config.ts
@@ -3,14 +3,14 @@ import { compilerOptions } from './tsconfig.json'
 
 const jestConfig: JestConfigWithTsJest = {
   verbose: true,
-  reporters: ['json-summary'],
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>'],
   modulePaths: [compilerOptions.baseUrl],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: '<rootDir>/'
-  })
+  }),
+  coverageReporters: ['json-summary']
 }
 
 export default jestConfig
